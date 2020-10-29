7f3185c46c9c2eab146a1b61436ac25e756645e4
Adi52 <adimr52@gmail.com>
2020-10-29 10:45:50 +0100

    Fix bugs with fonts

--- src/components/Duck.js
+++ src/components/Duck.js
@@ -20,6 +20,7 @@ export default class Duck {
             y: this.gameHeight,
         };
         this.flyAwayNow = false;
+        this.dropSoundActive = true;
 
         // images
         this.ducksFlyUpImage = document.querySelector('#ducksFlyUpImg');
